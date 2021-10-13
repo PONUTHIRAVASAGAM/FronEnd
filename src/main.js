dec41878d4f531bd2451b687cb62e58bc250641b
dexter <you@example.com>
2021-10-13 17:27:55 +0530

    msg

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VueLocalStorage from 'vue-localstorage' 
+Vue.use(VueLocalStorage)
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
